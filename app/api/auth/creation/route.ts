883ae21ace063fe59257b92b301900ccaca581db
YounsNayef <159211032+YounsNayef@users.noreply.github.com>
2024-07-20 15:54:56 +0200

    finish

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -6,51 +6,45 @@ import { unstable_noStore as noStore } from "next/cache";
 
 export async function GET() {
   noStore();
-  const { getUser } = getKindeServerSession();
-  const user = await getUser();
+  
+  try {
+    const { getUser } = getKindeServerSession();
+    const user = await getUser();
 
-  if (!user || user === null || !user.id) {
-    throw new Error("Something went wrong...");
-  }
+    if (!user || user === null || !user.id) {
+      throw new Error("User not authenticated or missing user ID");
+    }
 
-  let dbUser = await prisma.user.findUnique({
-    where: {
-      id: user.id,
-    },
-  });
-
-  if (!dbUser) {
-    const account = await stripe.accounts.create({
-      email: user.email as string,
-      controller: {
-        losses: {
-          payments: "application",
-        },
-        fees: {
-          payer: "application",
-        },
-        stripe_dashboard: {
-          type: "express",
-        },
-      },
+    let dbUser = await prisma.user.findUnique({
+      where: { id: user.id },
     });
 
-    dbUser = await prisma.user.create({
-      data: {
-        id: user.id,
-        firstName: user.given_name ?? "",
-        lastName: user.family_name ?? "",
-        email: user.email ?? "",
-        profileImage:
-          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
-        connectedAccountId: account.id,
-      },
-    });
-  }
+    if (!dbUser) {
+      const account = await stripe.accounts.create({
+        email: user.email as string,
+        type: 'express',
+      });
 
-  return NextResponse.redirect(
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:3000"
-      : "https://gardi-ui.vercel.app/"
-  );
+      dbUser = await prisma.user.create({
+        data: {
+          id: user.id,
+          firstName: user.given_name ?? "",
+          lastName: user.family_name ?? "",
+          email: user.email ?? "",
+          profileImage: user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+          connectedAccountId: account.id,
+        },
+      });
+    }
+
+    return NextResponse.redirect(
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:3000"
+        : "https://gardi-ui.vercel.app/"
+    );
+
+  } catch (error) {
+    console.error("Error during GET request:", error);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
