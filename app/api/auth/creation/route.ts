3831b6440be1737e700a5177142d920ad1e921e8
YounsNayef <159211032+YounsNayef@users.noreply.github.com>
2024-07-20 15:58:21 +0200

    finish

--- app/api/auth/creation/route.ts
+++ app/api/auth/creation/route.ts
@@ -6,45 +6,51 @@ import { unstable_noStore as noStore } from "next/cache";
 
 export async function GET() {
   noStore();
-  
-  try {
-    const { getUser } = getKindeServerSession();
-    const user = await getUser();
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
 
-    if (!user || user === null || !user.id) {
-      throw new Error("User not authenticated or missing user ID");
-    }
+  if (!user || user === null || !user.id) {
+    throw new Error("Something went wrong...");
+  }
 
-    let dbUser = await prisma.user.findUnique({
-      where: { id: user.id },
+  let dbUser = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) {
+    const account = await stripe.accounts.create({
+      email: user.email as string,
+      controller: {
+        losses: {
+          payments: "application",
+        },
+        fees: {
+          payer: "application",
+        },
+        stripe_dashboard: {
+          type: "express",
+        },
+      },
     });
 
-    if (!dbUser) {
-      const account = await stripe.accounts.create({
-        email: user.email as string,
-        type: 'express',
-      });
-
-      dbUser = await prisma.user.create({
-        data: {
-          id: user.id,
-          firstName: user.given_name ?? "",
-          lastName: user.family_name ?? "",
-          email: user.email ?? "",
-          profileImage: user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
-          connectedAccountId: account.id,
-        },
-      });
-    }
-
-    return NextResponse.redirect(
-      process.env.NODE_ENV === "development"
-        ? "http://localhost:3000"
-        : "https://gardi-ui.vercel.app/"
-    );
-
-  } catch (error) {
-    console.error("Error during GET request:", error);
-    return new NextResponse("Internal Server Error", { status: 500 });
+    dbUser = await prisma.user.create({
+      data: {
+        id: user.id,
+        firstName: user.given_name ?? "",
+        lastName: user.family_name ?? "",
+        email: user.email ?? "",
+        profileImage:
+          user.picture ?? `https://avatar.vercel.sh/${user.given_name}`,
+        connectedAccountId: account.id,
+      },
+    });
   }
+
+  return NextResponse.redirect(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : "https://gardi-ui.vercel.app/"
+  );
 }
